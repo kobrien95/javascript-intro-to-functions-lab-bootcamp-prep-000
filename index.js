f48d6aacb4f25bc6bd6571936e75b602d2008a70
Kristian O'Brien <kristianobrien95@gmail.com>
2019-02-17 20:29:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,5 +11,6 @@ function logWhisper(string){
   console.log('hello')
 }
 function sayHiToGrandma(string){
-  return string.toLowerCase("I can't hear you!")
+  return string.toLowerCase
+  console.log('I cant hear you!)
 }
